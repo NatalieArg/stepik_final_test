b701265c2a6b47701c0fbce27af3592077bcf436
NatalieArg <57195306+NatalieArg@users.noreply.github.com>
2019-12-12 20:36:55 +0300

    добавлена маркировка тестов, вынесена ссылка в тестах, добавлены комментарии к коду

--- test_product_page.py
+++ test_product_page.py
@@ -4,12 +4,13 @@ from .pages.basket_page import BasketPage
 import pytest
 import faker
 
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
 
 class TestGuestAddToBasketFromProductPage:
     @pytest.mark.parametrize('page_param', [0, 1, 2, 3, 4, 5, 6, pytest.param(7, marks=pytest.mark.xfail), 8, 9])
     def test_guest_can_add_product_to_basket_promo(self, browser, page_param):
-        link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{page_param}"
-        page = ProductPage(browser, link)
+        linkn = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{page_param}"
+        page = ProductPage(browser, linkn)
         page.open()  # открываем страницу
         page.should_add_product_to_basket()  # добавляем товар в корзину
         page.solve_quiz_and_get_code()  # вводим код
@@ -18,7 +19,6 @@ class TestGuestAddToBasketFromProductPage:
 
     @pytest.mark.need_review
     def test_guest_can_add_product_to_basket(self, browser):
-        link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
         page = ProductPage(browser, link)
         page.open()  # открываем страницу
         page.should_add_product_to_basket()  # добавляем товар в корзину
@@ -27,35 +27,30 @@ class TestGuestAddToBasketFromProductPage:
 
     @pytest.mark.xfail
     def test_guest_cant_see_success_message_after_adding_product_to_basket(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
         page = ProductPage(browser, link)
         page.open()  #  открываем страницу
         page.should_add_product_to_basket()  # добавляем товар в корзину
         page.should_not_be_message_add_product_to_basket()  # проверяем что нет сообщения о добавлении товара в корзину
 
     def test_guest_cant_see_success_message(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
         page = ProductPage(browser, link)
         page.open()  # открываем страницу товара
         page.should_not_be_message_add_product_to_basket()  # проверяем что нет сообщения о добавлении в корзину
 
     @pytest.mark.xfail
     def test_message_disappeared_after_adding_product_to_basket(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
         page = ProductPage(browser, link)
         page.open()  # открываем страницу
         page.should_add_product_to_basket()  # добавляем товар в корзину
         page.should_disappear_message_add_product_to_basket()  # проверяем что исчезает сообщение о добавлении товара
 
     def test_guest_should_see_login_link_on_product_page(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
         page = ProductPage(browser, link)
         page.open()  # открываем страницу
         page.should_be_login_link()  # проверяем что есть ссылка на страницу регистрации
 
     @pytest.mark.need_review
     def test_guest_can_go_to_login_page_from_product_page(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
         page = ProductPage(browser, link)
         page.open()  # открываем страницу
         page.go_to_login_page()  # выполняем метод страницы - переходим на страницу логина
@@ -64,7 +59,6 @@ class TestGuestAddToBasketFromProductPage:
 
     @pytest.mark.need_review
     def test_guest_cant_see_product_in_basket_opened_from_product_page(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
         page = ProductPage(browser, link)
         page.open()  # открываем страницу
         basket_page = BasketPage(browser, browser.current_url)
@@ -75,24 +69,22 @@ class TestGuestAddToBasketFromProductPage:
 class TestUserAddToBasketFromProductPage:
     @pytest.fixture(scope="function", autouse=True)
     def setup(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+        linkn = "http://selenium1py.pythonanywhere.com/accounts/login/"
         f = faker.Faker()
         email = f.email()  # генерируем тестовый email
         password = f.password()  # генерируем тестовы пароль
-        new_reg = LoginPage(browser, link)
+        new_reg = LoginPage(browser, linkn)
         new_reg.open()  # открываем страницу
         new_reg.register_new_user(email, password)  # регистрируем нового пользователя
         new_reg.should_be_authorized_user()  # проверяем что пользователь авторизован
 
     def test_user_cant_see_success_message(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
         page = ProductPage(browser, link)
         page.open()  # открываем страницу
         page.should_not_be_message_add_product_to_basket()  # проверяем что нет сообщения о добавлении в корзину
 
     @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
         page = ProductPage(browser, link)
         page.open()  # открываем страницу
         page.should_add_product_to_basket()  # добавляем товар в корзину
